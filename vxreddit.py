e892b00dcbcead03679641e9e52b9499c7bd1398
Ellie <ellie@bm90.dev>
2026-07-04 22:37:07 +0300

    only serve embeds to bots, redirect users

--- vxreddit.py
+++ vxreddit.py
@@ -4,6 +4,7 @@ import os
 import re
 import urllib.parse
 
+import crawleruseragents
 import m3u8
 import praw
 import prawcore
@@ -17,21 +18,14 @@ import videoCombiner
 app = Flask(__name__)
 CORS(app)
 
-"""
-facebookexternalhit/1.1
-Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36
-Mozilla/5.0 (Windows; U; Windows NT 10.0; en-US; Valve Steam Client/default/1596241936; ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36
-Mozilla/5.0 (Windows; U; Windows NT 10.0; en-US; Valve Steam Client/default/0; ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.4 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.4 facebookexternalhit/1.1 Facebot Twitterbot/1.0
-facebookexternalhit/1.1
-Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; Valve Steam FriendsUI Tenfoot/0; ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36
-Slackbot-LinkExpanding 1.0 (+https://api.slack.com/robots)
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0
-Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)
-TelegramBot (like TwitterBot)
-Mozilla/5.0 (compatible; January/1.0; +https://gitlab.insrt.uk/revolt/january)
-test
-"""
+
+bot_pattern = re.compile(
+    "|".join(
+        bot["pattern"]
+        for bot in crawleruseragents.CRAWLER_USER_AGENTS_DATA
+        if "social-preview" in bot["tags"]
+    )
+)
 
 r_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
@@ -445,9 +439,12 @@ def validate_path(path):
 
 @app.route("/<path:path>")
 def embedReddit(path):
-    path = clean_path(path)
+    is_bot = bot_pattern.search(request.headers.get("User-Agent", ""))
 
-    post_id, comment_id = validate_path(path)
+    if not is_bot:
+        return redirect("https://www.reddit.com/" + path)
+
+    post_id, comment_id = validate_path(clean_path(path))
 
     return embed_reddit(post_id, comment_id)
 
